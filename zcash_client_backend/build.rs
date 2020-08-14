3df0ffadc64c26aa3c35f9694579a0d176f59bc2
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-08-13 20:01:14 -0400

    Add local RPC protobuf types.
    If we stick with this route, we can probably just pull it over to the sdk's lib.rs file instead.

--- zcash_client_backend/build.rs
+++ zcash_client_backend/build.rs
@@ -3,7 +3,7 @@ use protobuf_codegen_pure;
 fn main() {
     protobuf_codegen_pure::Codegen::new()
         .out_dir("src/proto")
-        .inputs(&["proto/compact_formats.proto"])
+        .inputs(&["proto/compact_formats.proto","proto/local_rpc_types.proto"])
         .includes(&["proto"])
         .run()
         .expect("Protobuf codegen failed");
